7e50f337140b4a5f363fb9d5f1075341618e5d32
MatoG50 <matog50@gmail.com>
2024-02-06 20:14:55 +0300

    Added Modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { Text } from '@chakra-ui/react';
 import './App.css';
+import MyModal from './MyModal';
+import { useState } from 'react';
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+  const onClose = () => setIsOpen(false);
+  const onOpen = () => setIsOpen(true);
   return (
     <>
-      <h1>Home</h1>
+      <div className='container'>
+        <Text fontSize='4xl' fontWeight='bold'>
+          Notes
+        </Text>
+        <img src='./src/assets/add_icon.svg' alt='image' onClick={onOpen} />
+      </div>
+      <MyModal isOpen={isOpen} onClose={onClose} />
     </>
   );
 }
---
+++ src/MyModal.tsx
@@ -0,0 +1,36 @@
+import {
+  Modal,
+  Button,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from '@chakra-ui/react';
+
+const MyModal = ({ isOpen, onClose }) => {
+  return (
+    <>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            {/* Modal Content */}
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant='ghost'>Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default MyModal;
